ec6c4d14154cb5d4fa3448ecc1180a8539ccc476
nsavramenko <navramenko.work@gmail.com>
2023-03-13 14:08:12 -0200

    with new edits

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,17 +8,17 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
 
         HelloWorld helloWorldBean1 =
-                applicationContext.getBean(HelloWorld.class);
+                applicationContext.getBean("helloworld", HelloWorld.class);
 //        System.out.println(helloWorldBean1.getMessage());
         HelloWorld helloWorldBean2 =
-                applicationContext.getBean(HelloWorld.class);
+                applicationContext.getBean("helloworld",HelloWorld.class);
 
         System.out.print("Comparing of HelloWorld: " );
         System.out.println(helloWorldBean1 == helloWorldBean2);
 
 
-        Cat catBean1 = applicationContext.getBean(Cat.class);
-        Cat catBean2 = applicationContext.getBean(Cat.class);
+        Cat catBean1 = applicationContext.getBean("cat", Cat.class);
+        Cat catBean2 = applicationContext.getBean("cat", Cat.class);
 
         System.out.print("Comparing of Cat: " );
         System.out.println(catBean1 == catBean2);
